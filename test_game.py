2743864e0bc7590b7a8696f0750435229018bd5d
pabloga44 <67555151+pabloga44@users.noreply.github.com>
2022-09-20 16:15:40 +0200

    Update test_game.py

--- test_game.py
+++ test_game.py
@@ -1,3 +1,4 @@
+from venv import create
 import functions
 import sys
 
@@ -8,4 +9,28 @@ def test_input_coordinates():
     i,j = functions.input_coordinates()
     assert i in [0,1,2]
     assert j in [0,1,2]
+    return
 
+
+def test_move():
+    """Test a couple of movements in the board, check that it doesn't crash
+    when selecting an occupied square"""
+
+    board = functions.create_board()
+    board = functions.move(board,1,1)
+    
+    # Check that it doesn't accept other indexes
+    try:
+        functions.move(board,0,4)
+        raise Exception("Error in test move: move(0,4) did not fail.")
+    except IndexError:
+        pass
+
+    try:
+        board = functions.move(board,1,1)
+        raise Exception("Error in test move: didn't fail when trying to rewrite a square.")
+    
+    except functions.OccupiedSquare:
+        pass
+    board = functions.move(board,1,2)
+    return
