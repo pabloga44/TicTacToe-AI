5c43df369c74278e1740a534170e795974e43df3
pabloga44 <67555151+pabloga44@users.noreply.github.com>
2022-09-21 17:43:33 +0200

    Move every method to the classes

--- classes.py
+++ classes.py
@@ -1,4 +1,15 @@
 import numpy as np
+import random as rand
+
+class WrongBoard(Exception):
+    """A board that is impossible was reached."""
+    pass
+
+
+class OccupiedSquare(Exception):
+    """The selected squared has been already filled"""
+    pass
+
 
 class Board:
 
@@ -13,6 +24,24 @@ class Board:
     def __repr__(self) -> str:
         """Return string description."""
         return f"{type(self).__name__}\n(board={self.board})"
+
+    def set_board(self, new_board):
+        """Set manually the values of all the board.
+        This function should be private and never used by the user."""
+
+        # Check that it is an array
+        if (not isinstance(new_board, np.ndarray)):
+            raise WrongBoard("In set board - new board is not an array.")
+        # Check that it has the correct shape
+        elif (new_board.shape != (3,3)):
+            raise WrongBoard("In set board - wrong dimensions.")
+        # Check that it only contains valid values
+        elif (any(x not in [0,1,2] for x in np.nditer(new_board))):
+            raise WrongBoard("In set board - incorrect values in new board.")
+        # Assing the new board
+        else:
+            self.board = new_board
+
 
     def show(self):
         """Print board (only for 3x3)"""
@@ -32,4 +61,116 @@ class Board:
         print(f"    {A[2][0]} | {A[2][1]} | {A[2][2]}")
         print("\n")
 
-    
+    def move(self, i, j):
+        """Takes one board and a square and executes the correspondand move"""
+
+        # First check that square is avaliable
+        if (self.board[i,j] != 0):
+            raise OccupiedSquare("Error in move: selected square is already filled.")
+
+        # Calculate whose turn is it
+        nX = sum(sum(self.board == 1))
+        nO = sum(sum(self.board == 2))
+
+        if ( (nX - nO) == 1 ): turn = 2 # Turn for Os
+        elif ( nX == nO ): turn = 1 # Turn for Xs
+        else: raise WrongBoard("Error in move: incorrect board.")
+
+        # Fill square
+        self.board[i,j] = turn
+
+    def random_move(self):
+        """Move in a random empty square of the board. 
+        Note: different that previous random_move function, because doesn't return coordinates, but it directly moves."""
+
+        while(True):
+            # Generate random square
+            i = rand.randint(0,2)
+            j = rand.randint(0,2)
+
+            # If its empty we return it
+            if (self.board[i,j] == 0): 
+                self.move(i,j)
+
+    def get_winner(self):
+        """Return the winner of the board 1 o 2 (X o O)"""
+        
+        # Also check that there is no 2 or more lines
+        count = 0
+        winner = 0
+
+        # Columns and rows
+        for i in range(3):
+            
+            # Rows
+            if ((self.board[i,0] == self.board[i,1] == self.board[i,2]) & (self.board[i,0] != 0)): 
+                count += 1
+                winner = self.board[i,0]
+            # Columns
+            if((self.board[0,i] == self.board[1,i] == self.board[2,i]) & (self.board[0,i] != 0)): 
+                count += 1
+                winner = self.board[0,i]
+                
+        # Diagonals
+        if ((self.board[0,0] == self.board[1,1] == self.board[2,2]) & (self.board[1,1] != 0)): 
+                count += 1
+                winner = self.board[1,1]
+        if ((self.board[0,2] == self.board[1,1] == self.board[2,0]) & (self.board[1,1] != 0)): 
+                count += 1
+                winner = self.board[1,1]
+
+        # Check only one line
+        if count > 1: 
+            raise WrongBoard("Error in board_winner - more than one winner line.")
+        else:
+            return winner
+
+
+class GameCommunication:
+    """The GameCommunication class contains all the functions related with asking and showing information to the user during the game."""
+
+    def input_coordinates():
+        """Ask the user for a row and column to move"""
+
+        valid_i = False
+        while (not valid_i):
+            try: # In case the user doesn't introduce an integer
+                i = int(input("Enter your row: ")) - 1  # We already change it to 0-2 format
+                if (i in [0,1,2]): # In case the user introduces a different integer
+                    valid_i = True
+                else:
+                    print("Please enter a valid row (1,2,3)")
+            except ValueError: # If we enter not integer to int it raises ValueError
+                print("Please enter a valid row (1,2,3)")  
+                
+        valid_j = False
+        while (not valid_j):
+            try:
+                j = int(input("Enter your column: ")) - 1  # We already change it to 0-2 format
+                if (j in [0,1,2]):
+                    valid_j = True
+                else:
+                    print("Please enter a valid column (1,2,3)")
+            except ValueError:
+                print("Please enter a valid column (1,2,3)")
+
+        return i, j
+
+    def show_team_winner_end(winner):
+        """Ends the game by showing which team won and the credits"""
+        
+        if (winner == 1): print("X wins -- TO DO Say who won")
+        elif (winner == 2): print("O wins -- TO DO Say who won")
+        elif (winner == 0): print("Tie!")
+        else:
+            raise Exception("Winner is undefined")
+        print("\nBy Pablo Gallego Adrián.")
+
+    def show_winner_end(winner, player_team, name):
+        """Ends the game by showing who won and the credits"""
+        
+        if(winner == 0): print("It's a tie!")
+        elif (winner == player_team): print("Congratulations "+name+", you won!")
+        else: print("I'm sorry "+name+ ", but I won!")    
+
+        print("\nBy Pablo Gallego Adrián.")
--- test_classes.py
+++ test_classes.py
@@ -1,5 +1,6 @@
 # Classes tests
-from classes import Board
+from classes import *
+import sys
 
 def test_board_creation():
     """Check the that the instatiation of a board,
@@ -8,3 +9,98 @@ def test_board_creation():
     board = Board()
     print(board)
     board.show()
+    return
+
+
+def test_input_coordinates():
+    """Test the inputation of coordinates by the user"""
+
+    sys.stdin = open("test_inputs/test_input_coordinates.txt") # We choose the same name for organization
+    i,j =  GameCommunication.input_coordinates()
+    assert i in [0,1,2]
+    assert j in [0,1,2]
+    return
+
+
+def test_move():
+    """Test a couple of movements in the board, check that it doesn't crash
+    when selecting an occupied square"""
+
+    # Create board and first move
+    board = Board()
+    board.move(1,1)
+    
+    # Check that it doesn't accept other indexes
+    try:
+        board.move(0,4)
+        raise Exception("Error in test move: move(0,4) did not fail.")
+    except IndexError:
+        pass
+
+    # Check that it doesn't allow me to rewrite a square
+    try:
+        board.move(1,1)
+        raise Exception("Error in test move: didn't fail when trying to rewrite a square.")
+    except OccupiedSquare:
+        pass
+
+    # Another normal move
+    board.move(1,2)
+    return
+
+
+def test_set():
+    """Test the function set_board."""
+
+    # Create board
+    board = Board()
+
+    # Check it doesn't allow other values
+    try:
+        board.set_board(np.array([[8,1,1],[0,2,0],[2,3,2]]))
+        raise Exception("Test_set: it accepted a wrong value.")
+    except WrongBoard:
+        pass
+
+    # Check it doesn't accept other shapes
+    try:
+        board.set_board(np.array([[8,1],[0,2],[2,2]]))
+        raise Exception("Test_set: it accepted a wrong value.")
+    except WrongBoard:
+        pass
+
+    # Check it doesn't accept random things
+    try:
+        board.set_board("this is a test")
+        raise Exception("Test_set: it accepted something that was not an array.")
+    except WrongBoard:
+        pass
+
+    # Check that it does the assignation correctly
+    board.set_board(np.array([[0,2,1],[0,2,0],[2,0,2]]))
+    assert (board.board == np.array([[0,2,1],[0,2,0],[2,0,2]])).all()
+
+        
+
+def test_winner():
+    """Test the function that evaluates a board and return its winner (X or O)"""
+
+    board = Board()
+    board.set_board(np.array([[1,1,1],[0,2,0],[1,2,2]]))
+    assert board.get_winner() == 1
+
+    board.set_board(np.array([[2,1,1],[0,2,0],[1,2,2]]))
+    assert board.get_winner() == 2
+
+    board.set_board(np.array([[0,1,1],[0,2,0],[1,0,2]]))
+    assert board.get_winner() == 0
+
+    try:
+        board.set_board(np.array([[1,1,1],[0,2,0],[2,2,2]]))
+        w = board.get_winner()
+        raise Exception("Test_winner: it accepted a wrong board.")
+    except WrongBoard:
+        pass
+
+    return
+
